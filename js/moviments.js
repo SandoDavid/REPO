778777c17b3f1244be9d195a109c3d68e77bc6ed
SandoDavid <davidsandoval@ginebro.cat>
2021-02-01 18:08:52 +0100

    passaAnterior

---
+++ js/moviments.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function passaSeguent(objRebut){
+  let idObjPregAct = objRebut.parentElement.id; 
+  let objPregAct = document.getElementById(idObjPregAct);
+  let colArticles = document.getElementsByTagName("article");
+  let idObjPregSeg;
+    for (let i = 0; i < colArticles.length; i++) {
+    if(colArticles[i].id == idObjPregAct){
+      idObjPregSeg = colArticles[i+1].id;
+      break;
+    };
+  }
+  let objPregSeg = document.getElementById(idObjPregSeg);
+  objPregAct.classList.remove("elementVisible");
+  objPregAct.classList.add("elementOcult");
+  objPregSeg.classList.add("elementVisible");
+  objPregSeg.classList.remove("elementOcult");
+}
+
+
+function passaAnt(objRebut){
+  let idObjPregAct = objRebut.parentElement.id; 
+  let objPregAct = document.getElementById(idObjPregAct);
+  let colArticles = document.getElementsByTagName("article");
+  let idObjPregSeg;
+    for (let i = 0; i < colArticles.length; i++) {
+    if(colArticles[i].id == idObjPregAct){
+      idObjPregSeg = colArticles[i-1].id;
+      break;
+    };
+  }
+  let objPregSeg = document.getElementById(idObjPregSeg);
+  objPregAct.classList.remove("elementVisible");
+  objPregAct.classList.add("elementOcult");
+  objPregSeg.classList.add("elementVisible");
+  objPregSeg.classList.remove("elementOcult");
+}
